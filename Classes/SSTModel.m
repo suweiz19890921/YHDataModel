f9fa77e2e6994b8111b0a1654edd0cea3100c315
刘欢庆 <liu-lhq@163.com>
2017-04-21 21:48:12 +0800

    1.水温增加数据库支持

--- Classes/SSTModel.m
+++ Classes/SSTModel.m
@@ -9,5 +9,15 @@
 #import "SSTModel.h"
 
 @implementation SSTModel
+#pragma mark - 数据库模块
++ (nullable NSArray<NSString *> *)hq_propertyPrimarykeyList;
+{
+    return @[@"geohash"];
+}
 
+/** 所属库名称 该字段是生成数据库的必要字段*/
++ (nonnull NSString *)hq_dbName
+{
+    return @"service.db";
+}
 @end
